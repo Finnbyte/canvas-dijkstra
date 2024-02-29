71756d33e1ccf7e66ed9bde25677c00805d7fb04
Finnbyte <eelpy@protonmail.com>
2024-02-29 20:27:14 +0200

    Add colors of different things as constants

--- js/constants.js
+++ js/constants.js
@@ -14,3 +14,10 @@ export const GRID_ROWS_COLS_LENGTH = Math.floor(GRID_SIZE / CELL_SIZE);
  * The visualization's where draws each node the algorithm has visited frames per second
  */
 export const VISUALIZATION_FPS = 60;
+
+export const RESULT_PATH_NODE_COLOR = "orange";
+export const START_NODE_COLOR = "red";
+export const END_NODE_COLOR = "orange";
+export const VISITED_NODE_COLOR = "darkgreen";
+export const WALL_COLOR = "black";
+export const EMPTY_NODE_COLOR = "white";
